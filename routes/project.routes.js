7639b5c2acafb22f0c7627ffb6f7d5aeda9cd0eb
Pavitra <pandeypavitra65@gmail.com>
2023-11-03 19:53:44 +0530

    second commit

---
+++ routes/project.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// router.route("/save").post(saveProject)
+
+// router.route("/fetch").post(fetchProject)
+
+module.exports = router
+
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const connectDb = require('./config/dbconnection')
+const connectDb = require('./config/db.config')
 
 connectDb()
 
 const port = 5000
 
 app.use(express.json())
-app.use('/', require('./routes/projectRoutes.js'))
+app.use('/project', require('./routes/project.routes.js'))
 
 
 app.listen(port, () => {
